ede639d27c44867dcac92a2cb709f11114995df7
James Green <JamesTadleyGreen@outlook.com>
2023-08-08 23:29:50 +0100

    Seeting up random adn beinging benchmarking

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,11 @@
+import Criterion.Main
 import Data.Bits (FiniteBits (countTrailingZeros))
 import Data.Hashable (Hashable (hash))
-import Data.List (nub)
-import Date.Random.Extras (sample)
+import Data.List
+import System.Random
 
-integers = [1 ..]
-
-test = sample 100 integers
+randomList :: [Int] -> StdGen -> Int -> [Int]
+randomList l g n = take n $ randomRs (0, length l - 1) g
 
 deterministicCount :: [Int] -> Int
 deterministicCount = length . nub
@@ -14,5 +14,12 @@ probablisticCount :: [Int] -> Int
 probablisticCount l = maximum $ map (countTrailingZeros . hash) l
 
 main = do
-  print $ deterministicCount test
-  print $ probablisticCount test
+  seed <- getStdGen
+  let test = randomList [1 .. 1000000] seed 10000
+  print test
+  let probablistic = probablisticCount test
+  print probablistic
+  let deterministic = deterministicCount test
+  print deterministic
+  let accuracy = (fromIntegral deterministic) / (fromIntegral probablistic)
+  print accuracy
